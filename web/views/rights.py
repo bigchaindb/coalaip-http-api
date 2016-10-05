c11d1aaa01d5691d655dd94baace65c0ad23d8f6
Brett Sun <qisheng.brett.sun@gmail.com>
2016-10-05 13:37:00 +0200

    Implement Rights transfers

--- web/views/rights.py
+++ web/views/rights.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_restful import reqparse, Resource, Api
 
-from coalaip import CoalaIp
+from coalaip import CoalaIp, ModelDataError, entities
 from coalaip_bigchaindb.plugin import Plugin
 from web.models import right_model, user_model
 from web.utils import get_bigchaindb_api_url
@@ -42,4 +42,49 @@ class RightApi(Resource):
         return res
 
 
+class RightTransferApi(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('rightId', type=str, required=True,
+                            location='json')
+        parser.add_argument('currentHolder', type=user_model, required=True,
+                            location='json')
+        parser.add_argument('to', type=user_model, required=True,
+                            location='json')
+        parser.add_argument('rightsAssignment', type=dict, location='json')
+        args = parser.parse_args()
+
+        right_id = args['rightId']
+        current_holder = args['currentHolder']
+        to = args['to']
+        rights_assignment = args['rightsAssignment']
+
+        for user in [current_holder, to]:
+            user['public_key'] = user.pop('publicKey')
+            user['private_key'] = user.pop('privateKey')
+
+        # We can't be sure of the type of Right that's given by using just the
+        # id, so let's assume it's a normal Right first before trying to make a
+        # Copyright
+        try:
+            right = entities.Right.from_persist_id(right_id,
+                                                   plugin=coalaip.plugin,
+                                                   force_load=True)
+        except ModelDataError:
+            right = entities.Copyright.from_persist_id(right_id,
+                                                       plugin=coalaip.plugin,
+                                                       force_load=True)
+
+        res = coalaip.transfer_right(right=right,
+                                     rights_assignment_data=rights_assignment,
+                                     current_holder=current_holder,
+                                     to=to)
+
+        res = {'rightsAssignment': res.to_jsonld()}
+
+        return res
+
+
 right_api.add_resource(RightApi, '/rights', strict_slashes=False)
+right_api.add_resource(RightTransferApi, '/rights/transfer',
+                       strict_slashes=False)
