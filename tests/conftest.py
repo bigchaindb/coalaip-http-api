dae599a8954bcdd337dd367494eb626f9aadaa8b
Brett Sun <qisheng.brett.sun@gmail.com>
2016-10-05 13:37:00 +0200

    Implement Rights transfers

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,3 +18,30 @@ def alice(client):
 @pytest.fixture
 def bob(client):
     return client.post(url_for('user_views.userapi')).json
+
+
+@pytest.fixture
+def carly(client):
+    return client.post(url_for('user_views.userapi')).json
+
+
+@pytest.fixture
+def created_derived_right_with_mock_source(client, alice):
+    import json
+    from time import sleep
+
+    payload = {
+        'currentHolder': alice,
+        'right': {
+            'license': 'http://www.ascribe.io/terms',
+        },
+        'sourceRightId': 'mockId',
+    }
+
+    resp = client.post(url_for('right_views.rightapi'),
+                       data=json.dumps(payload),
+                       headers={'Content-Type': 'application/json'})
+
+    # Sleep for a bit to let the transaction become valid
+    sleep(3)
+    return resp.json['right']
