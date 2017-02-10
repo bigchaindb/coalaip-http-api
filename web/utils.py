f2fc4b50a993b5bea983b683c6f41ff939e7b8e1
Brett Sun <qisheng.brett.sun@gmail.com>
2017-02-10 12:12:13 +0100

    Remove settings and configuration for BDB_NODE_API_PATH as the driver does this for us now

--- web/utils.py
+++ web/utils.py
@@ -5,7 +5,6 @@ import os
 BigchainDBConfiguration = namedtuple('BigchainDBConfiguration', [
     'hostname',
     'port',
-    'api_path'
 ])
 
 
@@ -19,20 +18,14 @@ BDB_PORT = os.environ.get('BDB_NODE_PORT', None)
 if not BDB_PORT:
     BDB_PORT = '9984'
 
-BDB_API_PATH = os.environ.get('BDB_NODE_API_PATH', None)
-if not BDB_API_PATH:
-    BDB_API_PATH = 'api/v1'
-
 
 def get_bigchaindb_configuration():
-    return BigchainDBConfiguration(BDB_HOST, BDB_PORT, BDB_API_PATH)
+    return BigchainDBConfiguration(BDB_HOST, BDB_PORT)
 
 
 def get_bigchaindb_api_url():
-    hostname, port, api_path = get_bigchaindb_configuration()
-    return 'http://{hostname}:{port}/{api_path}'.format(hostname=hostname,
-                                                        port=port,
-                                                        api_path=api_path)
+    hostname, port = get_bigchaindb_configuration()
+    return 'http://{hostname}:{port}'.format(hostname=hostname, port=port)
 
 
 def parse_model(required_fields):
